a6fe8bc7464cf803868ebb0411d5b156401c5d34
Pablo Terceros <pablo.terceros.60104@usalesiana.edu.bo>
2024-03-08 10:14:04 -0400

    added labels

--- form/main.py
+++ form/main.py
@@ -13,23 +13,36 @@ root.title("Name Form with Options and Percentage")
 style = ttk.Style()
 style.theme_use('clam')
 
+# Function to create title labels
+def create_title_label(parent, text, row):
+    label = tk.Label(parent, text=text, font=('Helvetica', 12, 'bold'))
+    label.grid(row=row, column=0, columnspan=2, pady=(10, 5), sticky=tk.W)
+    return label
+
+# Create a title label for the entries
+create_title_label(root, "Entries:", row=0)
+
 # Create a list to store name entry frames and corresponding entry widgets
 name_entries = []
 for i in range(1, 5):
     name_entry_label = f"Name {i}:"
     name_entry_frame, name_entry = create_name_entry_frame(root, label_text=name_entry_label)
-    name_entry_frame.grid(row=i - 1, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+    name_entry_frame.grid(row=i, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
     name_entries.append((name_entry_label, name_entry))
 
-# Create frames for other modules
+# Create radio buttons
+create_title_label(root, "Radio Buttons:", row=5)
 radio_button_frame, option = create_radio_button_frame(root)
-drop_menu_frame, percentage_var = create_drop_menu_frame(root)
-button_frame = create_button_frame(root, name_entries, option, percentage_var)
+radio_button_frame.grid(row=6, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
-# Grid layout for frames
-radio_button_frame.grid(row=4, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-drop_menu_frame.grid(row=5, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-button_frame.grid(row=6, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+# Create drop down menu
+create_title_label(root, "Drop Menu:", row=7)
+drop_menu_frame, percentage_var = create_drop_menu_frame(root)
+drop_menu_frame.grid(row=8, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+# Create button
+button_frame = create_button_frame(root, name_entries, option, percentage_var)
+button_frame.grid(row=9, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
 # Start the main loop
 root.mainloop()
